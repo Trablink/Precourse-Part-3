be632029d53ff6c30af4c93a6cabe3b0dc4b044d
Jason Goldman <106274489+Trablink@users.noreply.github.com>
2022-11-07 11:42:41 -0600

    Precourse-Part-2 Partial

--- fundamentals/src/main.js
+++ fundamentals/src/main.js
@@ -3,8 +3,8 @@
 // const shallowClone = clone(users);
 // shallowClone[0] === users[0] → true
 function clone(value) {
-  // CODE HERE
-
+  const shallowClone = Object.assign({}, value)
+  return shallowClone;
 }
 
 
@@ -14,8 +14,10 @@ function clone(value) {
 // size([1,2,3]); → 3
 // size({a: 1, b: 2}); → 2
 function size(collection) {
-  // CODE HERE
-	
+  if (typeof collection === 'array') {
+    return (collection.length);
+  }
+  return (Object.keys(collection).length);
 }
 
 
@@ -24,8 +26,7 @@ function size(collection) {
 // first([1,2,3]); → 1
 // first([]); → undefined
 function first(array) {
-  // CODE HERE
-	
+  return (array[0]);
 }
 
 
@@ -35,8 +36,8 @@ function first(array) {
 // drop([1, 2, 3], 2); → [3]
 // drop([1, 2, 3], 5); → []
 // drop([1, 2, 3], 0); → [1, 2, 3]
-function drop(array, n) {
-  // CODE HERE
+function drop(array, n = 1) {
+  return (array.slice(n))
 	
 }
 
@@ -47,8 +48,8 @@ function drop(array, n) {
 // take([1, 2, 3], 2); → [1, 2]
 // take([1, 2, 3], 5); → [1, 2, 3]
 // take([1, 2, 3], 0); → []
-function take(array, n) {
-  // CODE HERE
+function take(array, n = 1) {
+  return (array.slice(0,n))
 	
 }
 
@@ -56,7 +57,11 @@ function take(array, n) {
 // Gets the value of key from all elements in collection.
 // pluck([{user: 'Bob', age: 20},{user: 'Sam', age: 25}], 'user'); → ['Bob','Sam']
 function pluck(array, key) {
-  // CODE HERE
+  const outputArr = [];
+  array.forEach (el => {
+    outputArr.push(el[key])
+  })
+  return outputArr
 	
 }
 
@@ -66,9 +71,12 @@ function pluck(array, key) {
 // extend({ 'user': 'barney' }, { 'age': 40 }, { 'user': 'fred' });
 // should return ->  { 'user': 'fred', 'age': 40 }
 // BONUS: solve with reduce
-function extend(destination) {
-  // CODE HERE
-	
+function extend(destination, ...sources) {
+  let outputObj = destination
+for (let j = 0; j < sources.length; j++) {
+  Object.assign(outputObj, sources[j])
+}
+return outputObj;
 }
 
 
@@ -85,22 +93,34 @@ const start = 2;
 applyAndEmpty(2, puzzlers); → 3
 */
 function applyAndEmpty(input, queue) {
-  // CODE HERE
-	
+  for (let i = 0; i < queue.length; i) {
+    let func = queue.shift()
+    input = func(input)
+  }
+	return input
 }
 
 
 // Returns a function that when called, will check if it has already computed
 // the result for the given argument and return that value instead if possible.
 function memoize(func) {
-  // CODE HERE
-	
+let memo = {};
+return (...args) => {
+  let n = args[0];
+  if (n in memo) {
+    return memo[n];
+  }
+  else {
+    let result = func(n);
+    memo[n] = result;
+    return result;
+  }
+}
 }
 
 
 // Invokes func after wait milliseconds. Any additional arguments are provided
 // to func when it is invoked.
-function delay(func, wait) {
-  // CODE HERE
-	
+function delay(func, wait, ...args) {
+  setTimeout(func, wait, ...args)
 }
